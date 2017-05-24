663d698af15ae0c89125bcc1c3f63adc3ceaaaa1
liukai <liukai9106@163.com>
2017-05-24 16:46:35 +0800

    init project

---
+++ UpdateWithKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UpdateWithKeyboard
+//
+//  Created by liukai on 24/05/2017.
+//  Copyright © 2017 com.liukai.updateWithKeyboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UpdateWithKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UpdateWithKeyboard
+//
+//  Created by liukai on 24/05/2017.
+//  Copyright © 2017 com.liukai.updateWithKeyboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
